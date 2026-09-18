79f32ffa1d3ed9095e3e87b3b45cda89673a520b
Beth <beth.williams@bluewin.ch>
2026-09-18 11:44:36 +0200

    simple matrix

--- cmd/led/main.go
+++ cmd/led/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("LED project is alive!")
+	height := 32
+	width := 32
+	matrix := make([][]int, height)
+	for i := 0; i < height; i++ {
+		matrix[i] = make([]int, width)
+		for j := 0; j < width; j++ {
+			matrix[i][j] = 0
+		}
+	}
+
+	printMatrix(matrix)
+}
+
+func printMatrix(matrix [][]int) {
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			fmt.Print(matrix[i][j], " ")
+		}
+		fmt.Println()
+	}
 }
